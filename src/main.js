1753c4ec67701c8cea386fd8618f0932091d2672
Derek Clark <Derekvc312@gmail.com>
2023-05-22 12:57:49 -0400

    Added a Dark mode theme that saves to local storage

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
+/* Lazy loading with priority listing */
+
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/featured.css";
+import "../styles/components/projects.css";
 import "../styles/utils.css";
 
+import darkMode from "./utils/dark-mode";
+darkMode();
 /*
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
